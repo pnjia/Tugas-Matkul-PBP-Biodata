1abe35471abe76e1a03996437bfcd05d5177f667
Panji Angkasa Putra <panjiangkasaputra67@gmail.com>
2024-09-20 18:29:17 +0700

    First committo

---
+++ app.js
@@ -0,0 +1,8 @@
+const nama = document.querySelector('#name');
+nama.textContent = "Panji Angkasa Putra";
+
+const ttl = document.querySelector('#ttl');
+ttl.textContent = "Sukabumi, 17 Juli 2006";
+
+const alamat = document.querySelector('#alamat');
+alamat.textContent = "Jalan Merdeka 2 no.38 rt03/04 Cikundul Hilir"; 
